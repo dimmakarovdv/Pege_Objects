e5b25283216228c6b0ae0c3ac35ccc64e4b4fff8
Neto test <dimmakarovdv89@mail.ru>
2025-10-31 21:55:12 +0500

    made changes to the file TransferPage.java and ManeyTransferTest.java

--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -1,17 +1,39 @@
 package ru.netology.page;
 
 import com.codeborne.selenide.Condition;
-
-import static com.codeborne.selenide.Selenide.*;
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
 
 public class TransferPage {
+    private SelenideElement amountInput = $("[data-test-id=amount] input");
+    private SelenideElement fromInput = $("[data-test-id=from] input");
+    private SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    private SelenideElement errorNotification = $("[data-test-id=error-notification]");
+    private SelenideElement cancelButton = $("[data-test-id=action-cancel]");
+
     public TransferPage() {
         $("[data-test-id=amount]").shouldBe(Condition.visible);
     }
-
-    public void makeTransfer(String amount, String fromCard) {
-        $("[data-test-id=amount] input").setValue(amount);
-        $("[data-test-id=from] input").setValue(fromCard);
-        $("[data-test-id=action-transfer]").click();
+    public DashboardPage makeSuccessfulTransfer(String amount, String fromCard) {
+        makeTransfer(amount, fromCard);
+        return new DashboardPage();
+    }
+    public TransferPage makeTransferWithError(String amount, String fromCard) {
+        makeTransfer(amount, fromCard);
+        checkErrorNotificationVisible();
+        return this;
+    }
+    public DashboardPage cancelTransfer() {
+        cancelButton.click();
+        return new DashboardPage();
+    }
+    private void makeTransfer(String amount, String fromCard) {
+        amountInput.setValue(amount);
+        fromInput.setValue(fromCard);
+        transferButton.click();
+    }
+    private void checkErrorNotificationVisible() {
+        errorNotification.shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка"));
     }
 }
--- src/test/java/ru/netology/test/ManeyTransferTest.java
+++ src/test/java/ru/netology/test/ManeyTransferTest.java
@@ -1,12 +1,12 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
+import ru.netology.page.TransferPage;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,8 +15,8 @@ public class ManeyTransferTest {
     private DataHelper.CardInfo firstCard;
     private DataHelper.CardInfo secondCard;
 
-    @Test
-    void shouldTransferMoneyBetweenOwnCards() {
+    @BeforeEach
+    void setup() {
         open("http://localhost:9999");
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
@@ -25,34 +25,28 @@ public class ManeyTransferTest {
         dashboardPage = verificationPage.validVerify(verificationCode);
         firstCard = DataHelper.getFirstCard();
         secondCard = DataHelper.getSecondCard();
+    }
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
         int firstCardBalance = dashboardPage.getCardBalance(firstCard);
         int secondCardBalance = dashboardPage.getCardBalance(secondCard);
         int amount = firstCardBalance / 2;
-        var transferPage = dashboardPage.selectCard(secondCard);
-        transferPage.makeTransfer(String.valueOf(amount), firstCard.getCardNumber());
+        TransferPage transferPage = dashboardPage.selectCard(secondCard);
+        dashboardPage = transferPage.makeSuccessfulTransfer(String.valueOf(amount), firstCard.getCardNumber());
         assertEquals(firstCardBalance - amount, dashboardPage.getCardBalance(firstCard));
         assertEquals(secondCardBalance + amount, dashboardPage.getCardBalance(secondCard));
     }
     @Test
     void shouldTransferMoneyBetweenCardsIfBalanceLimitExceeded() {
-        open("http://localhost:9999");
-        var loginPage = new LoginPage();
-        var authInfo = DataHelper.getAuthInfo();
-        var verificationCode = DataHelper.getVerificationCodeFor(authInfo).getCode();
-        var verificationPage = loginPage.validLogin(authInfo);
-        dashboardPage = verificationPage.validVerify(verificationCode);
-        firstCard = DataHelper.getFirstCard();
-        secondCard = DataHelper.getSecondCard();
         int firstCardBalance = dashboardPage.getCardBalance(firstCard);
         int secondCardBalance = dashboardPage.getCardBalance(secondCard);
-        int amount = firstCardBalance + 10000;
-        var transferPage = dashboardPage.selectCard(secondCard);
-        transferPage.makeTransfer(String.valueOf(amount), firstCard.getCardNumber());
-        $("[data-test-id=error-notification]").shouldBe(Condition.visible)
-                .shouldHave(Condition.text("Ошибка"));
+        int amount = firstCardBalance + 1000;
+                TransferPage transferPage = dashboardPage.selectCard(secondCard);
+        transferPage.makeTransferWithError(String.valueOf(amount), firstCard.getCardNumber());
+        dashboardPage = transferPage.cancelTransfer();
         assertEquals(firstCardBalance, dashboardPage.getCardBalance(firstCard),
-                "Баланс первой карты не должен измениться при этой ошибке");
+                "Баланс первой карты не должен измениться при ошибке перевода");
         assertEquals(secondCardBalance, dashboardPage.getCardBalance(secondCard),
-                "Баланс второй карты не должен измениться при этой ошибке");
+                "Баланс второй карты не должен измениться при ошибке перевода");
     }
 }
